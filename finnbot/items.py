08506e30c64c4086c49ede74a5f9a20839ac2fab
Vegard Nitter Vestad <vegard@vnvestad.com>
2019-10-14 19:51:19 +0200

    Add more value fields

--- finnbot/items.py
+++ finnbot/items.py
@@ -6,7 +6,34 @@ import scrapy
 
 class Ad(scrapy.Item):
     id = scrapy.Field()
+    title = scrapy.Field()
+
     item = scrapy.Field()
 
 class RealestateHome(scrapy.Item):
+    advertiser_reference = scrapy.Field()
+
+    cadastres_cadastral_unit_number = scrapy.Field()
+    cadastres_property_unit_number = scrapy.Field()
+    cadastres_county_number = scrapy.Field()
+
+    change_of_ownership_insurance = scrapy.Field()
+    construction_year = scrapy.Field()
+    energy_label = scrapy.Field()
+    energy_label_color_code = scrapy.Field()
+    general_text = scrapy.Field()
+    no_of_bedrooms = scrapy.Field()
+    ownership_type = scrapy.Field()
+    plot_area = scrapy.Field()
+    plot_owned = scrapy.Field()
+    property_type = scrapy.Field()
+    size_gross = scrapy.Field()
+    size_primary = scrapy.Field()
+    size_usable = scrapy.Field()
+    viewings = scrapy.Field()
+
     estimated_value = scrapy.Field()
+    loan_fare = scrapy.Field()
+    community_tax = scrapy.Field()
+    tax_value = scrapy.Field()
+    common_costs = scrapy.Field()
--- finnbot/spiders/finn.py
+++ finnbot/spiders/finn.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.loader import ItemLoader
 from finnbot.items import Ad, RealestateHome
+import re
 
 class FinnSpider(scrapy.Spider):
     name = 'finn'
@@ -18,9 +20,20 @@ class FinnSpider(scrapy.Spider):
 
     def parse_ad(self, response):
         ad = Ad(
-            id = response.css('span[data-adid]::attr(data-adid)').get()
+            id = int(response.css('span[data-adid]::attr(data-adid)').get())
         )
         ad['item'] = RealestateHome(
-            estimated_value = int("".join(response.css('span:contains("Prisantydning") + span').re(r'\d')))
+            estimated_value = price2int(response.css('span:contains("Prisantydning") + span::text').get()),
+            no_of_bedrooms = response.css('dt:contains("Soverom") + dd::text').get(),
+            ownership_type = response.css('dt:contains("Eieform") + dd::text').get(),
+            plot_area = response.css('dt:contains("Tomteareal") + dd::text').get(),
+            property_type = response.css('dt:contains("Boligtype") + dd::text').get(),
+            size_gross = response.css('dt:contains("Bruttoareal") + dd::text').get(),
+            size_primary = response.css('dt:contains("Primærrom") + dd::text').get(),
+            size_usable = response.css('dt:contains("Bruksareal") + dd::text').get(),
+            common_costs = price2int(response.css('dt:contains("Felleskost/mnd.") + dd::text').get())
         )
         yield ad
+
+def price2int(price):
+    return int("".join(re.findall(r'\d', price))) if price else None
